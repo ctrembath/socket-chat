d04e821f70861a1915273da0e6c325d692faeb03
ctrembath <claretrembath@ymail.com>
2014-12-11 19:52:25 +0000

    terminal reports users disconnection

--- src/socketController.js
+++ src/socketController.js
@@ -2,7 +2,12 @@ var socket = function(io){
 
   io.on('connection', function(socket){
     console.log('user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
+});
 };
+
+
 
 module.exports = socket;
